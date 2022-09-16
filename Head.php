49286932caf2b1c91751883c48561d4ed2350de9
NamicUV <98799956+NamicUV@users.noreply.github.com>
2022-09-16 11:39:31 -0400

    Add files via upload
    Start

---
+++ Head.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title><? echo $pageTitle ?></title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="senior.css">
+	<script src="senior.js"></script>
+</head>
+
+<body>
+<div class="navbar">
+  <a href="https://dabettercheddar.com/Main.php">Home</a>
+  <a href="https://dabettercheddar.com/hiw.php">How It Works</a>
+  <a href="https://dabettercheddar.com/support.php">Support</a>
+ <div class="topnav-right">
+    <a href="https://dabettercheddar.com/login.php">Login</a>
+	 
+ </div>	  
+</div>
---
+++ Main.php
@@ -0,0 +1,9 @@
+
+<?
+$pageTitle = "Homepage";//change for each page
+include_once("Head.php");
+?>
+	
+<body>
+</body>
+</html>
