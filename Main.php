dac12151a78c93c7bc67a6cce5930694e78ec6e7
NamicUV <98799956+NamicUV@users.noreply.github.com>
2022-09-21 08:50:35 -0400

    Add files via upload
    Html Layout Added

--- Main.php
+++ Main.php
@@ -3,8 +3,31 @@
 $pageTitle = "Homepage";//change for each page
 include_once("Head.php");
 ?>
-	
-<body>
-	
+<link rel="stylesheet" href="Main.css">
+
+<section class="s1">
+<br>
+<div>
+	<img src="r.jpg" class="content">
+	<img src="logo.png" class="img">
+</div>
+	<p class="space">...</p>
+</section>
+
+<section class="s2">
+<div>
+	<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Insert_image_here.svg/299px-Insert_image_here.svg.png" class="img2">
+</div>
+	<h1 class="txt">There's no better time to start a budget than today. Our website is a user friendly tool that will help you organize your finances, lower your debt, and save you money all at the same time.</h1>
+</section>
+<br>
+<section class="s3">
+	<h1 class="txt2">There's no better time to start a budget than today. Our website is a user friendly tool that will help you organize your finances, lower your debt, and save you money all at the same time.</h1>
+	<div>
+		<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Insert_image_here.svg/299px-Insert_image_here.svg.png" class="img3">
+	</div>
+</section>
+
+
 </body>
 </html>
--- hiw.php
+++ hiw.php
@@ -1,8 +1,25 @@
 <?
-$pageTitle = "Homepage";//change for each page
+$pageTitle = "How It Works";//change for each page
 include_once("Head.php");
 ?>
-	
-<body>
+<link rel="stylesheet" href="hiw.css">
+
+<h1 class="title">How It Works</h1>
+
+<h2>
+What to expect from Better Cheddar
+</h2>
+	<div>
+	Better Cheddar was made with the intent to help reduce the percentage of people living paycheck to paycheck by allowing people to create a monthly budget. Users of Better Cheddar will also experience a decrease in their debt if any is present as well as an increase in money saved.
+	</div>
+<h2>
+No Need For Card Information
+</h2>
+	<div>
+	Unlike typical budgeting applications, Better Cheddar does not require the user to insert any card information to set up an account. Better Cheddar is free to use and only requires an account with Better Cheddar to use.
+	</div>
+<h2>
+Set Up Your Account In 3 Easy Steps
+</h2>
 </body>
 </html>
