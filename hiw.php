1776742d2160132ea33b7860d6c68c7ad9b90857
NamicUV <98799956+NamicUV@users.noreply.github.com>
2022-09-22 00:02:49 -0400

    Add files via upload
    How It Works (added CSS)

--- hiw.php
+++ hiw.php
@@ -6,20 +6,53 @@ include_once("Head.php");
 
 <h1 class="title">How It Works</h1>
 
-<h2>
+<h2 class="h1">
 What to expect from Better Cheddar
 </h2>
-	<div>
+	<div class="info">
 	Better Cheddar was made with the intent to help reduce the percentage of people living paycheck to paycheck by allowing people to create a monthly budget. Users of Better Cheddar will also experience a decrease in their debt if any is present as well as an increase in money saved.
 	</div>
-<h2>
+<h2 class="h2">
 No Need For Card Information
 </h2>
-	<div>
+	<div class="info">
 	Unlike typical budgeting applications, Better Cheddar does not require the user to insert any card information to set up an account. Better Cheddar is free to use and only requires an account with Better Cheddar to use.
 	</div>
-<h2>
+<h2 class="step">
 Set Up Your Account In 3 Easy Steps
 </h2>
+<div class="steps">
+
+  	<div class="box">
+		<a class="img">
+			<img src="https://cdn-icons-png.flaticon.com/512/2037/2037710.png" alt="Sign-In" />
+		</a>
+			<h2>Create an Account</h2>
+	  				<p>Create a free account and let us help you deal with your money</p>
+  	</div>
+  
+	<div class="box">
+		<a class="img">
+      		<img src="https://cdn1.iconfinder.com/data/icons/school-icons-2/512/note_list_choice_survey-512.png" alt="Regular Mini" />
+    	</a>
+			<h2>Answer Questions</h2>
+	  			<p>During your sign-up you will need to answer some questions to see how you want to allocate your money</p>
+	</div>
+  
+  	<div class="box">
+		<a class="img">
+      		<img src="https://cdn-icons-png.flaticon.com/512/1580/1580920.png" alt="Countryman Mini" />
+      	</a>
+			<h2>Start Your Budget</h2>
+	  			<p>Get 24/7 access to your budget for free and track your progress for the month </p>
+  	</div>
+</div>
+
+	<h2 class="h3">
+	Track Your Spending
+	</h2>
+		<div class="info">
+		Once a member, you will be able to see the progress of your spending through a display that changes color based on how good your standing is in your chosen budget plan.
+		</div>
 </body>
 </html>
