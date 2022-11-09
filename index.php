76de11925e9cca190312c7f97fb90081fa8bdb89
NamicUV <98799956+NamicUV@users.noreply.github.com>
2022-11-09 18:29:10 -0500

    Add files via upload
    Creation of Admin Page

--- index.php
+++ index.php
@@ -5,8 +5,10 @@ session_start();
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("login.php");
-    exit;
 }
+
+// Include config file
+require_once "server.php";
 ?>
  
 <!DOCTYPE html>
@@ -22,18 +24,117 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 	<p class="logout">
         <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
     </p>
-    	<h1 class="hello">Hello, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Here is your budget.</h1>
-    		<div class="row1">
-  				<div class="column left" style="background-color:#aaa;">
-    				<h2 class="acct">Account</h2>
-    				<a href="myplan.php" class="my1">My Plan</a><br>
-					<a href="mybudget.php" class="my1">My Budget</a><br>
-					<a href="myhistory.php" class="my1">My History</a>
-  				</div>
-  					<div class="column right" style="background-color:#bbb;">
-    					<h2 class="month">This Month</h2>
-    					<p>Some text..</p>
-  					</div>
-			</div>
+	<h1 class="text-center">Admin View</h1>
+	<div class="card">
+		<div class="card-body">
+			<h3>Users</h3>
+			<? 	$username = $_SESSION["username"];
+				$sql= "SELECT * FROM users";
+				$query_run = mysqli_query($link, $sql);
+			?>
+					<table id="table" class="table table-dark">
+						<thead>
+							<tr>
+								<th scope="col">Username</th>
+								<th scope="col">Needs</th>
+								<th scope="col">Savings</th>
+								<th scope="col">Wants</th>
+								<th>
+								</th>
+							</tr>
+						</thead>
+			<?php
+				if($query_run){
+				foreach($query_run as $row){
+			?>
+						<tbody>
+							<tr>
+								<td><?php echo $row['username']; ?></td>
+								<td><?php echo $row['needs']; ?></td>
+								<td><?php echo $row['savings']; ?></td>
+								<td><?php echo $row['wants']; ?></td>
+							</tr>
+							</tbody>
+			<?php
+				}
+				} else{
+				echo "No Record Found";
+				}
+			?>
+</table>
+			<h3>Budget Data</h3>
+			<? 	$username = $_SESSION["username"];
+				$sql= "SELECT * FROM list";
+				$query_run = mysqli_query($link, $sql);
+			?>
+					<table id="table" class="table table-dark">
+						<thead>
+							<tr>
+								<th scope="col">Username</th>
+								<th scope="col">Item Name</th>
+								<th scope="col">Item Cost</th>
+								<th scope="col">Item Type</th>
+								<th>
+								</th>
+							</tr>
+						</thead>
+			<?php
+				if($query_run){
+				foreach($query_run as $row){
+			?>
+						<tbody>
+							<tr>
+								<td><?php echo $row['username']; ?></td>
+								<td><?php echo $row['buy']; ?></td>
+								<td><?php echo $row['pay']; ?></td>
+								<td><?php echo $row['type']; ?></td>
+							</tr>
+							</tbody>
+			<?php
+				}
+				} else{
+				echo "No Record Found";
+				}
+			?>
+</table>
+			<h3>Income</h3>
+			<? 	$username = $_SESSION["username"];
+				$sql= "SELECT * FROM income";
+				$query_run = mysqli_query($link, $sql);
+			?>
+					<table id="table" class="table table-dark">
+						<thead>
+							<tr>
+								<th scope="col">Username</th>
+								<th scope="col">Income</th>
+								<th scope="col"> EDIT </th>
+                                <th scope="col"> DELETE </th>
+							</tr>
+						</thead>
+			<?php
+				if($query_run){
+				foreach($query_run as $row){
+			?>
+						<tbody>
+							<tr>
+								<td><?php echo $row['username']; ?></td>
+								<td><?php echo $row['income']; ?></td>
+								<td>
+                                    <button type="button" class="btn btn-warning editbtn"> EDIT </button>
+                                </td>
+                                <td>
+                                    <button type="button" class="btn btn-danger deletebtn"> DELETE </button>
+                                </td>
+							</tr>
+							</tbody>
+			<?php
+				}
+				} else{
+				echo "No Record Found";
+				}
+			?>
+</table>
+		</div>
+	</div>
 </body>
 </html>
